87bd3d694c785ccf0f710eebe0bf40f8251bc36e
Fambegbe Olamileke <olamileke.f@gmail.com>
2019-07-29 13:25:53 +0100

    Changed Home Page Design + made totally responsive

--- Pluto/__init__.py
+++ Pluto/__init__.py
@@ -1,13 +1,16 @@
 import os
 from flask import Flask, render_template
+from . import auth
 
 def create_app(test_config=None):
 	app=Flask(__name__, instance_relative_config=True)
 
 	app.config.from_mapping(SECRET_KEY='dev')
+	app.register_blueprint(auth.bp)
 
 	@app.route('/')
 	def index():
 		return render_template('index.html')
 
 	return app
+
---
+++ Pluto/auth.py
@@ -0,0 +1,10 @@
+from flask import render_template, Blueprint, request
+
+bp=Blueprint('auth', __name__)
+
+@bp.route('/signup', methods=('GET', 'POST'))
+def signup():
+	if request.method == 'POST':
+		print('leke')
+
+	return render_template('auth/signup.html')
