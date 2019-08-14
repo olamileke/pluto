e2389e2cf6ba44bd7d5a8468eb214702a5598d00
Fambegbe Olamileke <olamileke.f@gmail.com>
2019-08-14 22:02:28 +0100

    Added Edit Page

--- Pluto/projects.py
+++ Pluto/projects.py
@@ -14,17 +14,14 @@ allowedExtensions = ['jpg', 'jpeg', 'png']
 @authMiddleware
 def new():
     if request.method == 'POST':
-        if validateProjectInfo(request.form, request.files):
-            image=request.files['image']
-            filename=str(time.time()) + secure_filename(image.filename)
-            UPLOADS_FOLDER=join(current_app.config['BASEDIR'], 'pluto', 'static', 'images', 'Projects')
-            image.save(join(UPLOADS_FOLDER,filename))
+        if validateProjectInfo(request.form, request.files, True):
 
-            if request.form['github_link'] == '':
-            	request.form['github_link']=None
+            github_link = request.form['github_link']
+            if github_link == '':
+                github_link = None
 
-            project=Project(name=request.form['name'], about=request.form['about'], user_id=session['user_id'],
-            				picture=filename,github_link=request.form['github_link'])
+            project = Project(name=request.form['name'], about=request.form['about'], user_id=session['user_id'],
+                              picture=uploadImage(request.files['image']), github_link=github_link)
             db.session.add(project)
             db.session.commit()
             flash('Project created successfully', 'success')
@@ -36,18 +33,58 @@ def new():
 @bp.route('/<int:id>/<slug>')
 @authMiddleware
 def view(id, slug):
-    project=Project.query.filter((Project.id == id)).first()
+    project = Project.query.filter((Project.id == id)).first()
+
+    # checking if the authenticated user is the project owner
+    if project.user_id != session['user_id']:
+        flash('You do not have access', 'error')
+        return redirect(url_for('index'))
+
     return render_template('projects/view.html', project=project)
+
 
 @bp.route('')
 @authMiddleware
 def all():
-    projects=Project.query.filter((Project.user_id == session['user_id'])).all()
+    projects = Project.query.filter(
+        (Project.user_id == session['user_id'])).all()
     return render_template('projects/all.html', projects=projects)
 
 
+@bp.route('/edit/<int:id>', methods=('GET', 'POST'))
+@authMiddleware
+def edit(id):
+    if request.method == 'POST':
+        if validateProjectInfo(request.form, request.files, False):
+            project = Project.query.filter((Project.id == id)).first()
 
-def validateProjectInfo(form, files):
+            project.name = request.form['name']
+            project.about = request.form['about']
+            github_link = request.form['github_link']
+
+            if github_link == '':
+                github_link = None
+
+            project.github_link = github_link
+
+            if request.files['image'].filename != '':
+                project.picture = uploadImage(request.files['image'])
+
+            db.session.commit()
+            flash('Project updated!', 'success')
+            return redirect(url_for('projects.view', id=id, slug=project.name.lower().replace(' ', '-')))
+
+    project=Project.query.filter((Project.id == id)).first()
+
+    # checking if the authenticated user is the project owner
+    if project.user_id != session['user_id']:
+        flash('You do not have access', 'error')
+        return redirect(url_for('index'))
+
+    return render_template('projects/edit.html', project=project)
+
+
+def validateProjectInfo(form, files, required):
     if len(form['name']) < 5:
         flash('Project name must be at least 5 characters long', 'error')
         return False
@@ -56,20 +93,30 @@ def validateProjectInfo(form, files):
         flash('Select an appropriate image', 'error')
         return False
 
-    image = files['image']
+    if required is True:
+        image=files['image']
 
-    if image.filename == '':
-        flash('Select an appropriate image', 'error')
-        return False
+        if image.filename == '':
+            flash('Select an appropriate image', 'error')
+            return False
 
-    extension = image.filename.rsplit('.')[1].lower()
+        extension=image.filename.rsplit('.')[1].lower()
 
-    if extension not in allowedExtensions:
-        flash('File format is not supported', 'error')
-        return False
+        if extension not in allowedExtensions:
+            flash('File format is not supported', 'error')
+            return False
 
     if len(form['about']) < 5:
         flash('Project description must be at least 5 characters long', 'error')
         return False
 
     return True
+
+
+def uploadImage(image):
+    filename=str(time.time()) + secure_filename(image.filename)
+    UPLOADS_FOLDER=join(
+        current_app.config['BASEDIR'], 'pluto', 'static', 'images', 'Projects')
+    image.save(join(UPLOADS_FOLDER, filename))
+
+    return filename
---
+++ Pluto/static/js/Projects/edit.js
@@ -0,0 +1,111 @@
+"use strict"
+
+$(document).ready(function() {
+
+
+	// Image Preview Logic
+
+	let form=$('.edit-form');
+	let uploadbtn=form.find('button.upload');
+	let fileInput=form.find('#fileInput');
+	let imgParent=form.find('.image-parent');
+	let imageTypes=['jpg','jpeg','png','JPEG','PNG','JPG'];
+
+	uploadbtn.click(function() {
+
+		fileInput.click();
+	})
+
+
+	fileInput.change(function() {
+
+		imgParent.removeClass('active');
+		previewImage();
+	})
+
+
+	function previewImage() {
+
+		if(fileInput.prop('files').length > 0) {
+
+			if(validateFile(fileInput.prop('files')[0])) {
+
+				imgParent.addClass('active');
+				let reader=new FileReader();
+
+				reader.onload= function(e) {
+
+					imgParent.find('.project-img').attr('src', e.target.result);
+				}
+
+				reader.readAsDataURL(fileInput.prop('files')[0]);
+			}
+		}
+	}
+
+
+	function validateFile(file) {
+
+		let types=file.type.split('/');
+
+		if(types.length == 1) {
+
+			toastr.error("Invalid file format");
+			return false;
+		}
+
+		if(imageTypes.indexOf(types[1].toLowerCase()) == -1) {
+
+			toastr.error('Invalid file format');
+			return false;
+		}
+
+		if(file.size > 1200000) {
+
+			toastr.error('Selected image is too large');
+			return false;
+		}
+
+		return true;
+	}
+
+
+	// validation logic
+
+	let elements=form.find('.elements');
+	let imgContainer=$('.img-container');
+
+	form.submit(function(e) {
+
+		form.find('.error-notif').remove();
+		if(!validate()) {
+
+			e.preventDefault();
+		}
+	})
+
+
+	function validate() {
+
+		return validateInputs();
+	}
+
+
+	function validateInputs() {
+
+		let param=true;
+
+		elements.each(function() {
+
+			let $this=$(this);
+
+			if($this.val().length < 5) {
+
+				param=false;
+				$this.after("<p class='error-notif m-0 mt-2'>* Must be at least 5 characters in length</p>");
+			}
+		})
+
+		return param;
+	}
+})
